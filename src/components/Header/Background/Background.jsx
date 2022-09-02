4b7c2f03c7b7ef99970103886a4272577769a9c5
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2022-09-01 23:24:34 -0300

    img change

--- src/components/Header/Background/Background.jsx
+++ src/components/Header/Background/Background.jsx
@@ -2,8 +2,6 @@
 export function Background() { 
     const img = {maxHeight: '100vh', minHeight: '100vh', width: '100%'}
     return (
-        <div className="mt-5">
-            <img src='fotos/bodegon.jpeg' alt='background' style={img}/>
-        </div>
+        <div id="background"></div>
     )
 }
